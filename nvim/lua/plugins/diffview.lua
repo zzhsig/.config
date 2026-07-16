25913a996a926bfe721ca6ca986ac6e3fdc53392
zzhsig <zhaoheng.zhang@sigmatrx.ai>
2026-07-16 02:18:06 -0700

    Add diffview.nvim for branch-wide diffs
    Lazy-loaded on Diffview commands with <leader>gv (open) and <leader>gV (file history) keymaps.

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    cmd = {
+      "DiffviewOpen",
+      "DiffviewClose",
+      "DiffviewToggleFiles",
+      "DiffviewFocusFiles",
+      "DiffviewFileHistory",
+    },
+    keys = {
+      { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+      { "<leader>gV", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview File History" },
+    },
+    opts = {},
+  },
+}
